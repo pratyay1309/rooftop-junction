57f28be0332e1889a6a54f097b60dafb9d476f22
Rishi Raj Pratyay <pratyay1309@gmail.com>
2026-04-21 05:20:55 +0530

    modification for deployment

--- chatbot-backend/main.py
+++ chatbot-backend/main.py
@@ -70,8 +70,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
